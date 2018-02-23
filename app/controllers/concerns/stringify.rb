de97f264640f5a123942623cf81fb1e4dd4ad1f5
michellefan <fan.michellec@gmail.com>
2018-02-22 17:09:05 -0800

    yay

--- app/controllers/concerns/stringify.rb
+++ app/controllers/concerns/stringify.rb
@@ -7,6 +7,10 @@ class Stringify
   end
 
   def what_am_i
-  	# YOUR IMPLEMENTATION HERE
+    if !@name and !@adjective
+      "You are nothing!"
+    else
+      @name + " is so " + @adjective
+    end
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'pages#home'
+  post 'stringify', to: 'pages#stringify'
   get 'age', to: 'pages#age'
+  put 'age', to: 'pages#person'
 end
